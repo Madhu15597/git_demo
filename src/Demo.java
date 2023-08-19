a043c4f943a3ad5045162cb16373756f0a42b85f
jacksparrow <madhuvr515@gmail.com>
2023-08-19 06:13:36 +0530

    test

--- src/Demo.java
+++ src/Demo.java
@@ -4,7 +4,7 @@ public class Demo {
 	public static void main(String args[])
 	{
 		//testing
-		System.out.println("hello guys");
+		System.out.println("hello guys ok");
 		System.out.println("second changes");
 	}
 }
